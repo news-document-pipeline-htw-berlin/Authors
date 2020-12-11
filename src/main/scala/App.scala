e7fb9abd7ba82d0dd0eccd57653e11345fd418cb
staffano <>
2020-12-11 15:42:39 +0100

    mapped sentiments to authors

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,6 +1,11 @@
 import authorMapping.{Authors, Scoring}
+import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.config.ReadConfig
 import db.DBConnector
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.{DataFrame, SparkSession}
+
 
 object App {
 
@@ -30,6 +35,7 @@ object App {
 
 
   def main(args: Array[String]): Unit = {
+
     val inputMap = getConnectionInfoFromFile("src/main/resources/inputDBSettings")
     val outputMap = getConnectionInfoFromFile("src/main/resources/outputDBSettings")
 
@@ -49,20 +55,27 @@ object App {
       .config("spark.mongodb.output.uri", outputUri)
       .getOrCreate()
 
+    val readConfig = DBConnector.createReadConfig(inputUri)
+    val writeConfig = DBConnector.createWriteConfig(outputUri)
+    val rConfig = DBConnector.createReadConfig(DBConnector.createUri("127.0.0.1", "nlp", "test"))
 
-    val readConfig = DBConnector.createReadConfig(inputUri, spark)
-    val writeConfig = DBConnector.createWriteConfig(outputUri, sparkSession = spark)
     val mongoData = DBConnector.readFromDB(sparkSession = spark, readConfig = readConfig)
+    val nlpData = DBConnector.readFromDB(sparkSession = spark, readConfig = rConfig).drop("StopWordsCleaner", "document", "entities", "embeddings", "lemmatizer", "ner", "normalized", "sentence", "text", "token")
+
+    // Have to cast objectID to String because of NLP DB
+    val mergedData = joinDataFrames(mongoData.withColumn("_id", col("_id").cast(StringType)), nlpData)
 
     // Mapping elements
-    val groupedAuthors = Authors.groupByAuthorRDDRow(mongoData)
+    val groupedAuthors = Authors.groupByAuthorRDDRow(mergedData.rdd)
     val amountOfSourcesPerAuthor = Authors.averageSourcesPerAuthor(groupedAuthors)
-    val publishedOnDay = Authors.publishedOnDayRDD(groupedAuthors)
+    val publishedOnDay = Authors.amountOfArticlesPerDay(groupedAuthors)
     val perWebsite = Authors.amountOfArticlesByWebsiteRDD(groupedAuthors)
     val averageWordsPerArticle = Authors.averageWordsPerArticleRDD(groupedAuthors)
     val amountOfArticles = Authors.amountOfArticlesPerAuthor(groupedAuthors)
-    val perDepartment = Authors.articlesPerDepartment(groupedAuthors)
-    val lastTexts = Authors.lastNTexts(groupedAuthors,5)
+    val perDepartment = Authors.amountOfArticlesPerDepartment(groupedAuthors)
+    val lastTexts = Authors.lastNTexts(groupedAuthors, 5)
+    val sentimentPerCategory = Authors.avgSentimentPerDepartment(groupedAuthors)
+    val sentimentPerDay = Authors.avgSentimentPerDay(groupedAuthors)
 
     // Creation of Dataframes
     val articles = spark.createDataFrame(amountOfArticles.collect()).toDF("_id", "articles")
@@ -72,7 +85,8 @@ object App {
     val perDepartmentDF = spark.createDataFrame(perDepartment.collect()).toDF("_id", "perDepartment")
     val amountSourceDF = spark.createDataFrame(amountOfSourcesPerAuthor.collect()).toDF("_id", "avgAmountOfSources")
     val lastTextsDF = spark.createDataFrame(lastTexts).toDF("_id", "lastTexts")
-
+    val sentimentPerCategoryDF = spark.createDataFrame(sentimentPerCategory).toDF("_id", "sentimentPerDepartment")
+    val sentimentPerDayDF = spark.createDataFrame(sentimentPerDay).toDF("_id", "sentimentPerDay")
 
     // Trust score for authors
     val defaultScore = Scoring.giveAuthorDefaultScore(groupedAuthors.map(x => x._1))
@@ -80,17 +94,19 @@ object App {
     val scoreAfterAmountOfArticles = Scoring.reduceByAmountOfArticles(scoreAfterSources, amountOfArticles, spark)
 
 
-    // joining Dataframes
+    //joining Dataframes
     val joinedArticles = joinDataFrames(articles, averageWords)
     val joinedPublishedWebsite = joinDataFrames(daysPublished, perWebsiteDF)
     val joinedPublishedDepartment = joinDataFrames(joinedPublishedWebsite, perDepartmentDF)
     val joinedSourcePublished = joinDataFrames(joinedPublishedDepartment, amountSourceDF)
     val joinedScorePublished = joinDataFrames(scoreAfterAmountOfArticles, joinedSourcePublished)
-    val joinedLastTexts = joinDataFrames(joinedScorePublished,lastTextsDF)
-    val fullDataFrame = joinDataFrames(joinedArticles, joinedLastTexts)
+    val joinedLastTexts = joinDataFrames(joinedScorePublished, lastTextsDF)
+    val joinedSentimentDepartment = joinDataFrames(joinedLastTexts, sentimentPerCategoryDF)
+    val joinedSentimentPerDay = joinDataFrames(joinedSentimentDepartment, sentimentPerDayDF)
+    val fullDataFrame = joinDataFrames(joinedArticles, joinedSentimentPerDay)
+
     // save to MongoDB
     DBConnector.writeToDB(fullDataFrame, writeConfig = writeConfig)
-
 
   }
 }
--- src/main/scala/authorMapping/Authors.scala
+++ src/main/scala/authorMapping/Authors.scala
@@ -19,9 +19,9 @@ object Authors {
    If no published date is provided, crawl time will be used instead
   */
 
-  def groupByAuthorRDDRow(data: RDD[Row]): RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))] = {
+  def groupByAuthorRDDRow(data: RDD[Row]): RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))] = {
     data.flatMap(x => x.getAs[mutable.WrappedArray[String]](1).toList.map(y => (y, (x.getString(15), if (x.get(13) != null) x.getTimestamp(13) else x.getTimestamp(2), x.getString(12),
-      x.getAs[mutable.WrappedArray[String]](9).toList, x.getAs[mutable.WrappedArray[String]](3).toList))))
+      x.getAs[mutable.WrappedArray[String]](9).toList, x.getAs[mutable.WrappedArray[String]](3).toList, x.getString(17).toDouble))))
   }
 
 
@@ -30,7 +30,7 @@ object Authors {
    e.g. (Anna Krueger, 20)
   */
 
-  def amountOfArticlesPerAuthor(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))]): RDD[(String, Int)] = {
+  def amountOfArticlesPerAuthor(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Int)] = {
     data.map(x => (x._1, 1)).reduceByKey(_ + _)
   }
 
@@ -39,36 +39,59 @@ object Authors {
    e.g Map (Anna Krueger -> 306.66)
   */
 
-  def averageWordsPerArticleRDD(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))]): RDD[(String, Double)] = {
-    data.mapValues(x => (x._1.replace(",", "").split(split_regex).length.toDouble, 1.0)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).map(x => (x._1, x._2._1 / x._2._2))
+  def averageWordsPerArticleRDD(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Double)] = {
+    data.mapValues(x => (x._1.replace(",", "").split(" ").length.toDouble, 1.0)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).map(x => (x._1, x._2._1 / x._2._2))
   }
 
   /*
-   Maps every Authors to its average amount of sources used over all articles
+   Maps every author to its average amount of sources used over all articles
    e.g Map[Anna Krueger -> 1.61]
   */
 
-  def averageSourcesPerAuthor(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))]): RDD[(String, Double)] = {
+  def averageSourcesPerAuthor(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Double)] = {
     data.mapValues(x => (x._4.size, 1.0)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues(y => y._1 / y._2)
   }
 
 
   /*
-  Maps every Author to another Map with (key,value) => (Day the article was published, amount of times an article was published on this day)
-  e.g. Map[Anna Krueger -> Map(Thu -> 2, Fri -> 5)]
+  Maps every author to an Array (Day, Amount) => (Day the article was published, amount of times an article was published on this day)
+  e.g. RDD[(Anna Krueger -> Array((Thursday,2), (Friday, 5)))]
+  Days are ordered from Monday to Sunday
  */
-  def publishedOnDayRDD(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))]): RDD[(String, Array[(String, Int)])] = {
+
+  def amountOfArticlesPerDay(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Array[(String, Int)])] = {
     val x = data.mapValues(x => x._2).groupByKey().map(x => (x._1, x._2.toList.map(x => OffsetDateTime.ofInstant(Instant.ofEpochMilli(x.getTime), ZoneId.of("Z")).getDayOfWeek)))
     x.map(x => (x._1, x._2.map(y => (y.getValue, y, x._2.count(_ == y))).sortBy(x => x._1).map(x => (x._2.getDisplayName(TextStyle.FULL, Locale.ENGLISH), x._3)).distinct.toArray))
   }
 
   /*
    Maps every Author to another Map with (key,value) => (Name of category, amount of times an article was published in this category)
-   e.g. Map[Anna Krueger -> Map(Wissen -> 25, Reisen -> 2)]
+   e.g. RDD[(Anna Krueger, Array((Wissen,2), (Reisen, 5)))]
   */
 
-  def articlesPerDepartment(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))]): RDD[(String, Array[(String, Int)])] = {
+  def amountOfArticlesPerDepartment(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Array[(String, Int)])] = {
     data.mapValues(_._5).groupByKey().map(x => (x._1, x._2.flatten.map(y => (y, x._2.flatten.count(_ == y))).toArray.distinct))
+  }
+
+  /*
+    The same as articlesPerDeparment, just that it maps the average sentiment per department and not the amount per Department
+     e.g. RDD[(Anna Krueger, Array((Wissen,3.16), (Reisen, -1.1)))]
+   */
+
+  def avgSentimentPerDepartment(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Array[(String, Double)])] = {
+    data.mapValues(x => (x._5, x._6)).groupByKey().mapValues(y => y.flatMap(z => z._1.map(f => (f, z._2))).groupBy(_._1).map(x => (x._1, x._2.map(z => z._2).sum / x._2.size)).toArray)
+
+  }
+
+  /*
+    The same als publishedOnDay, just that it maps the average sentiment per Day and not the amount of articles per Day
+    e.g. RDD[(Anna Krueger, Array((Thursday,-1.13), (Friday, 0.25)))]
+   */
+
+  def avgSentimentPerDay(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Array[(String, Double)])] = {
+    val x = data.mapValues(x => (x._2, x._6)).groupByKey().mapValues(x => (x.map(y => (OffsetDateTime.ofInstant(Instant.ofEpochMilli(y._1.getTime), ZoneId.of("Z")).getDayOfWeek, y._2))))
+    x.mapValues(z => z.groupBy(_._1).map(y => (y._1.getValue, y._1.getDisplayName(TextStyle.FULL, Locale.ENGLISH), y._2.map(f => f._2).map(z => z).sum / y._2.size)).toArray.sortBy(_._1).map(x => (x._2, x._3)).distinct)
+
   }
 
 
@@ -77,12 +100,17 @@ object Authors {
    e.g. Map(Anna Krueger -> Map(sz -> 22, taz -> 3))
   */
 
-  def amountOfArticlesByWebsiteRDD(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))]): RDD[(String, Map[String, Double])] = {
+  def amountOfArticlesByWebsiteRDD(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))]): RDD[(String, Map[String, Double])] = {
     data.mapValues(x => x._3).groupByKey().map(x => (x._1, x._2.toList.map(y => (y, x._2.count(_ == y).toDouble)).toMap))
   }
 
-  def lastNTexts(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String]))], amount: Int): RDD[(String, Array[(String, Int)])] = {
-    data.mapValues(x => (x._1.replace(",", "").split(split_regex).length, OffsetDateTime.ofInstant(Instant.ofEpochMilli(x._2.getTime), ZoneId.of("Z"))))
+  /*
+    Maps the last N texts written by an author, sorted by date
+    e.g. Array(("Anna Krueger", Array(("2020-12-03", 11), ("2020-12-05", 10)))
+   */
+
+  def lastNTexts(data: RDD[(String, (String, java.sql.Timestamp, String, List[String], List[String], Double))], amount: Int): RDD[(String, Array[(String, Int)])] = {
+    data.mapValues(x => (x._1.replace(",", "").split(" ").length, OffsetDateTime.ofInstant(Instant.ofEpochMilli(x._2.getTime), ZoneId.of("Z"))))
       .mapValues(_.swap).groupByKey().mapValues(x => x.toList.sortBy(_._1).takeRight(amount).map(x => (x._1.toLocalDate.toString, x._2)).toArray)
   }
 
